2272513ff7f881426e6ce869f841b25b8dfc2416
Aslan <berezg86@gmail.com>
2020-01-28 20:42:44 +0300

    Верстка шапки

--- src/static/js/common.js
+++ src/static/js/common.js
@@ -1,3 +1,32 @@
-$(document).ready(function(){
-    alert(1);
+$(document).ready(function() {
+    // custom select *
+    $('.city-select select').select2();
+
+    // mobile menu *
+    $('.burger').click(function() {
+        $('.main-nav').fadeToggle();
+        $(this).toggleClass('active');
+    });
+    $(window).resize(function() {
+        var width = $(window).width();
+        if (width >= 992) {
+            $('.main-nav').removeAttr('style');
+        } else {
+            $(document).click(function(event) {
+                if ($(event.target).closest(".burger").length) return;
+                if ($(event.target).closest(".main-nav").length) return;
+                $(".main-nav").fadeOut();
+                event.stopPropagation();
+            });
+        }
+    });
+
+
+
+
+
+
+
+
+
 });
